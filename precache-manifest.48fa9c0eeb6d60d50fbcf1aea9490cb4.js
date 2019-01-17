fa809cec78ab0ff373ed6f9fb5a8fdb559e02256
Michel Paupulaire <mpaupulaire4@gmail.com>
2019-01-17 16:41:34 -0700

    Updates

--- precache-manifest.48fa9c0eeb6d60d50fbcf1aea9490cb4.js
+++ precache-manifest.48fa9c0eeb6d60d50fbcf1aea9490cb4.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/digi-db/static/js/runtime~main.369fedb2.js"
   },
   {
-    "revision": "f46f0e3d4a617bc88951",
-    "url": "/digi-db/static/js/main.f46f0e3d.chunk.js"
+    "revision": "4bc1fd0bf0ce692e9561",
+    "url": "/digi-db/static/js/main.4bc1fd0b.chunk.js"
   },
   {
     "revision": "68112636e05432620614",
     "url": "/digi-db/static/js/1.68112636.chunk.js"
   },
   {
-    "revision": "fddfed72f3835fa79595d44b15013c40",
+    "revision": "73f1868cc94d22a66d2f94a4bbe5ac26",
     "url": "/digi-db/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/digi-db/precache-manifest.1607a32637edafd0b178320a90fa693c.js"
+  "/digi-db/precache-manifest.48fa9c0eeb6d60d50fbcf1aea9490cb4.js"
 );
 
 workbox.clientsClaim();
