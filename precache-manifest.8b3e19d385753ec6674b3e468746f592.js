bcf8550b540b78633204c1bfe5043e5fd5347f05
Michel Paupulaire <mpaupulaire4@gmail.com>
2019-01-16 20:21:18 -0700

    Updates

---
+++ precache-manifest.8b3e19d385753ec6674b3e468746f592.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "369fedb25003b3aa40be",
+    "url": "/digi-db/static/js/runtime~main.369fedb2.js"
+  },
+  {
+    "revision": "5261551f09cd39e0d00f",
+    "url": "/digi-db/static/js/main.5261551f.chunk.js"
+  },
+  {
+    "revision": "523c42973e051c63b160",
+    "url": "/digi-db/static/js/1.523c4297.chunk.js"
+  },
+  {
+    "revision": "6c512011b781f3bb6a691346df589785",
+    "url": "/digi-db/index.html"
+  }
+];
